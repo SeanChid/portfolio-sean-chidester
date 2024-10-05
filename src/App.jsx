552182b4ec8b5d400304f7d4266003e77bef1bc2
SeanChid <seanchidester737@gmail.com>
2024-10-04 18:02:24 -0600

    started projects section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from './components/Header.jsx'
 import Home from './components/Home.jsx'
 import Skills from './components/Skills.jsx'
+import Projects from './components/Projects.jsx'
 import './App.css'
 
 function App() {
@@ -20,11 +21,11 @@ function App() {
 
       <div id='projects' className='section-pd first-bg'>
         <h1>Projects</h1>
-        <p>My projects will go here</p>
+        <Projects />
       </div>
 
       <div id='contact' className='section-pd second-bg'>
-        <h1>Contact Section</h1>
+        <h1>Contact Me</h1>
         <p>contact me here I guess</p>
       </div>
     </div>
---
+++ src/components/Projects.jsx
@@ -0,0 +1,14 @@
+const Projects = () => {
+    return (
+        <div className='projects-pos'>
+            <div className='skybooking-img'>
+                <text className='projects-text'>Sky Booking Here</text>
+            </div>
+            <div className='adoptahobby-img'>
+                <text className='projects-text'>Adopt a Hobby Here</text>
+            </div>
+        </div>
+    )
+}
+
+export default Projects
