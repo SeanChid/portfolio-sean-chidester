676f7b5f879ca849dab5295cda4050334a473047
SeanChid <seanchidester737@gmail.com>
2024-10-06 21:42:23 -0600

    added to projects section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,10 @@
 const Home = () => {
     return (
         <div id='text'>
-            <h5>Hello, I'm Sean Chidester. I'm a web developer/ software engineer in Lehi, Utah.</h5>
+            <h5>
+                Hello, I'm Sean. I'm a web developer/ software engineer in Lehi, Utah.
+                I'm proficient in React.js, Redux, PostgreSQL, and more.
+            </h5>
         </div>
     )
 }
