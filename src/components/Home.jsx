ed824b4e5214de47984913cc931fc19f902c3c42
SeanChid <seanchidester737@gmail.com>
2024-10-12 20:13:47 -0600

    added link buttons to homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,16 @@
 const Home = () => {
     return (
-        <div id='text'>
-            <h5>
-                Hello, I'm Sean. I'm a web developer/ software engineer in Lehi, Utah.
-                I'm proficient in React.js, Redux, PostgreSQL, and more.
-            </h5>
+        <div className='home-pos'>
+            <div className='intro-txt'>
+                <p>
+                    Hello, I'm Sean. I'm a web developer/ software engineer in Lehi, Utah.
+                    I'm proficient in React.js, Redux, PostgreSQL, and more.
+                </p>
+            </div>
+            <div className='link-btns'>
+                <button className='btn btn-primary'>GitHub</button>
+                <button className='btn btn-primary'>LinkedIn</button>
+            </div>
         </div>
     )
 }
